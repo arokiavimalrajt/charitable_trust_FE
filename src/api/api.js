d8fe49d75f2b0574822d2f37bef56fc518d33c51
Vimal Raj <vimal.r@venusgeo.com>
2026-03-27 19:17:03 +0530

    270320260717

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Create an Axios instance
 const api = axios.create({
   baseURL:
-    "https://charitable-trust-be-vimal.apps.le0z4hd8.centralindia.aroapp.io", // Set your base URL here
+    "https://charitable-trust-be-git-vimal.apps.le0z4hd8.centralindia.aroapp.io", // Set your base URL here
 });
 
 export default api;
